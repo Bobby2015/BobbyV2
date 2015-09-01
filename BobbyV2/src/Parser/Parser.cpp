c25d46f8f49ad2ce78dd7543a774400ab9f7f507
robobooga <robobooga@gmail.com>
2015-09-01 14:42:49 +0800

    SLAP parser code

--- BobbyV2/src/Parser/Parser.cpp
+++ BobbyV2/src/Parser/Parser.cpp
@@ -20,6 +20,15 @@ int Parse::add(unsigned x, unsigned y)
 	return x + y;
 }
 
+void Parse::addNewLine(string from, string to, string *content)
+{
+	size_t start_pos = 0;
+	while ((start_pos = content->find(from, start_pos)) != std::string::npos) {
+		content->replace(start_pos, from.length(), to);
+		start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+	}
+}
+
 bool Parse::concreteSyntaxValidator(string contents) {
 
 	/*
@@ -30,6 +39,8 @@ bool Parse::concreteSyntaxValidator(string contents) {
 		3. Use regex to extract entities and relationships
 	*/
 
+	string *contentPointer = &contents;
+
 
 	// Remove new line
 	string::size_type pos1 = 0; // Must initialize
@@ -38,14 +49,9 @@ bool Parse::concreteSyntaxValidator(string contents) {
 	}
 
 	// Add newline to {
-	string from("{");
-	string to("{\n");
-	size_t start_pos = 0;
-	while ((start_pos = contents.find(from, start_pos)) != std::string::npos) {
-		contents.replace(start_pos, from.length(), to);
-		start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
-	}
+	addNewLine("{", "{\n", contentPointer);
 
+	
 	// Add newline to }
 	string from1("}");
 	string to1("}\n");
@@ -81,31 +87,28 @@ bool Parse::concreteSyntaxValidator(string contents) {
 	size_t pos = 0;	// initial position
 	string token; // individual item after split
 
+	
+	regex procedure("^\\s*procedure\\s*(\\w{1,})\\s*\\{\\s*");
+	regex ifstmt("^\\s*if\\s+(\\w{1,})\\s+then\\s+\\{\\s*");
+	regex elsestmt("^\\s*else\\s+(\\{)\\s*");
+	regex whilestmt("^\\s*while\\s+(\\w{1,})\\s+\\{\\s*");
+	regex call("^\\s*call\\s+(\\w{1,})\\s*\\;\\s*");
+	regex assign("^\\s*(\\w{1,})\\s*=\\s*([^=]*)\\;\\s*");
+	regex end("^\\s*(\\})\\s*");
+
+
+
 	while ((pos = contents.find(delimiter)) != string::npos) {
 
 		token = contents.substr(0, pos);
 
 		smatch procedure_matches;
-		regex procedure("^\\s*procedure\\s*(\\w{1,})\\s*\\{\\s*");
-
 		smatch ifstmt_matches;
-		regex ifstmt("^\\s*if\\s+(\\w{1,})\\s+then\\s+\\{\\s*");
-
 		smatch else_matches;
-		regex elsestmt("^\\s*else\\s+(\\{)\\s*");
-
 		smatch while_matches;
-		regex whilestmt("^\\s*while\\s+(\\w{1,})\\s+\\{\\s*");
-
 		smatch call_matches;
-		regex call("^\\s*call\\s+(\\w{1,})\\s*\\;\\s*");
-
 		smatch assign_matches;
-		regex assign("^\\s*(\\w{1,})\\s*=\\s*([^=]*)\\;\\s*");
-
 		smatch end_matches;
-		regex end("^\\s*(\\})\\s*");
-
 
 		/*
 		for (auto result : procedure_matches) {
@@ -129,23 +132,17 @@ bool Parse::concreteSyntaxValidator(string contents) {
 		*/
 		if (!procedure_matches.empty()) {
 			cout << "[Parse found procedure] " + string(procedure_matches[0]) << endl;
-		}
-		if (!ifstmt_matches.empty()) {
+		} else if (!ifstmt_matches.empty()) {
 			cout << "[Parse found if] " + string(ifstmt_matches[0]) << endl;
-		}
-		if (!else_matches.empty()) {
+		} else if (!else_matches.empty()) {
 			cout << "[Parse found else] " + string(else_matches[0]) << endl;
-		}
-		if (!while_matches.empty()) {
+		} else if (!while_matches.empty()) {
 			cout << "[Parse found while] " + string(while_matches[0]) << endl;
-		}
-		if (!call_matches.empty()) {
+		} else if (!call_matches.empty()) {
 			cout << "[Parse found call] " + string(call_matches[0]) << endl;
-		}
-		if (!assign_matches.empty()) {
+		} else if (!assign_matches.empty()) {
 			cout << "[Parse found assign] " + string(assign_matches[0]) << endl;
-		}
-		if (!end_matches.empty()) {
+		} else if (!end_matches.empty()) {
 			cout << "[Parse found end] " + string(end_matches[0]) << endl;
 		}
 
@@ -153,4 +150,5 @@ bool Parse::concreteSyntaxValidator(string contents) {
 	}
 
 	return true;
-}
+} 
+
--- BobbyV2/src/Parser/Parser.h
+++ BobbyV2/src/Parser/Parser.h
@@ -8,6 +8,7 @@
 #include <regex>
 #include <locale>
 
+using namespace std;
 class Parse
 {
 	public:
@@ -16,12 +17,14 @@ class Parse
 		int add(unsigned x, unsigned y);
 
 		// validate concrete syntax of the source code, return true if valid syntax, otherwise false
-		bool concreteSyntaxValidator(std::string);
+		bool concreteSyntaxValidator(string);
 
 	private:
 		unsigned first;
 		unsigned second;
 		
+		void addNewLine(string from, string to, string *content);
+
 }; // Parse
 
 #endif  // PARSER_PARSER_H
