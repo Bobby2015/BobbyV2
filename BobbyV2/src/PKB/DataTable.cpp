91c4c0b3df9affe39c12b17f7a2b04790c906d5d
robobooga <robobooga@gmail.com>
2015-09-02 17:33:24 +0800

    Added basic support and structure of TableWrapper and var tables

--- BobbyV2/src/PKB/DataTable.cpp
+++ BobbyV2/src/PKB/DataTable.cpp
@@ -1,44 +1,49 @@
 #include "DataTable.h"
 
+using namespace DataTables;
+//------------------------//
+// TableWrapper functions //
+//------------------------//
+
+TableWrapper::TableWrapper()
+{
+}
+
+VarMapTable TableWrapper::getVMapTable()
+{
+	return vMapTable;
+}
+
+VarTable TableWrapper::getVarTable()
+{
+	return vTable;
+}
+
+//-----------------------//
+// VarMapTable functions //
+//-----------------------//
+
+VarMapTable::VarMapTable()
+{
+}
+VarMapTable::~VarMapTable()
+{
+}
+
+const map<string, int> VarMapTable::getMap()
+{
+	return varMap;
+}
+
+
 VarTable::VarTable()
 {
-
 
 }
 
 VarTable::~VarTable()
 {
 
-
-
 }
 
 
-
-
-ProcedureTable::ProcedureTable()
-{
-
-
-
-}
-
-ProcedureTable::~ProcedureTable()
-{
-
-
-}
-
-StatementTable::StatementTable()
-{
-
-
-
-}
-
-StatementTable::~StatementTable()
-{
-
-
-
-}
